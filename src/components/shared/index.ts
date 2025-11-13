c28ba7b67c35b2ad811c66b370669ebb61535bcf
jndolou <j.ndolou@gmail.com>
2025-11-13 16:12:48 +0100

    Added DiagnosticsSection.tsx

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,3 +1,4 @@
 export { GoogleRating } from "./GoogleRating";
 export { ExpertiseSection } from "./ExpertiseSection";
 export { FeaturedArticles } from "./FeaturedArticles";
+export { DiagnosticsSection } from "./DiagnosticsSection";
