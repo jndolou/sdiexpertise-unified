c074c8db2d29ddd688eb965094546871e3354127
jndolou <j.ndolou@gmail.com>
2025-11-13 12:55:27 +0100

    Updated ProPage.tsx

---
+++ src/components/sections/UneExpertiseReconnueSubsection/index.ts
@@ -0,0 +1,1 @@
+export { UneExpertiseReconnueSubsection } from "./UneExpertiseReconnueSubsection";
--- src/screens/ProPage/ProPage.tsx
+++ src/screens/ProPage/ProPage.tsx
@@ -9,6 +9,7 @@ import { NewsletterSubsection } from "../../components/sections/NewsletterSubsec
 import { PourquoiCrerUnSubsection } from "../../components/sections/PourquoiCrerUnSubsection";
 import { RencontronsNousSubsection } from "../../components/sections/RencontronsNousSubsection";
 import { TabBarSubsection } from "../../components/sections/TabBarSubsection";
+import { UneExpertiseReconnueSubsection } from "../../components/sections/UneExpertiseReconnueSubsection";
 
 export const ProPage = (): JSX.Element => {
   return (
@@ -28,6 +29,7 @@ export const ProPage = (): JSX.Element => {
         <PourquoiCrerUnSubsection />
         <ActualitsPourLesSubsection />
         <RencontronsNousSubsection />
+        <UneExpertiseReconnueSubsection />
         <NewsletterSubsection />
         <FooterSubsection />
       </main>
