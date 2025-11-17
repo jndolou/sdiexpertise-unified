9dd5ff465e0bc286ff8400af9b2673d04b276d99
jndolou <j.ndolou@gmail.com>
2025-11-17 13:08:01 +0100

    Updated FeaturedArticles.tsx

--- src/components/shared/FeaturedArticles.tsx
+++ src/components/shared/FeaturedArticles.tsx
@@ -163,7 +163,7 @@ export const FeaturedArticles = ({
         {showButton && (
           <div className="flex flex-col h-12 items-center justify-center gap-2.5 w-full">
             <div className="flex flex-col items-end gap-2.5 w-full">
-              <Link to="/services">
+              <Link to="/actualites">
                 <Button className="h-auto inline-flex items-center justify-center gap-2 px-4 py-3 bg-[#faf8fc] rounded-[62px] overflow-hidden shadow-[inset_1.13e-16px_1.85px_1.85px_#ffffff,inset_-1.13e-16px_-1.85px_1.85px_#ebebeb,3.63e-16px_2.93px_11.87px_#33333324] hover:bg-[#f5f3f7] border-0">
                   <span className="[font-family:'Ubuntu',Helvetica] font-bold text-[#1c1b1b] text-base tracking-[0] leading-[19.2px] whitespace-nowrap">
                     {buttonText}
