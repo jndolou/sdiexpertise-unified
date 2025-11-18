fa0de28b032b9c8bca33697d8e05b3eeacc0b5c0
jndolou <j.ndolou@gmail.com>
2025-11-18 07:47:51 +0100

    Updated CommandePack.tsx

--- src/screens/CommandePack/CommandePack.tsx
+++ src/screens/CommandePack/CommandePack.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { Button } from "../../components/ui/button";
 import { ActionButtonsSection } from "./sections/ActionButtonsSection";
 import { DiagnosticDetailsSection } from "./sections/DiagnosticDetailsSection";
-import { HeaderSection } from "./sections/HeaderSection";
 import { MainContentSection } from "./sections/MainContentSection";
 import { OverviewSection } from "./sections/OverviewSection";
 import { PricingSection } from "./sections/PricingSection";
@@ -14,9 +13,7 @@ import { SummarySection } from "./sections/SummarySection";
 export const CommandePack = (): JSX.Element => {
   return (
     <div className="overflow-hidden bg-[#f3f1f7] w-full min-w-[393px] flex flex-col relative">
-      <HeaderSection />
-
-      <main className="flex flex-col w-full items-start gap-3 pt-[18px] pb-8 px-4 mt-[114px]">
+      <main className="flex flex-col w-full items-start gap-3 pt-[18px] pb-8 px-4 mt-[18px]">
         <nav className="flex w-full items-center justify-between gap-4 relative">
           <Button
             variant="ghost"
