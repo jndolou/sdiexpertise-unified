9dbece0da965784f473cfcf8671fb7c330f5f7a0
jndolou <j.ndolou@gmail.com>
2025-11-19 00:04:43 +0100

    Updated Commande.tsx

--- src/screens/Commande/Commande.tsx
+++ src/screens/Commande/Commande.tsx
@@ -60,16 +60,6 @@ export const Commande = (): JSX.Element => {
         <DiagnosticResultsSection />
         <ProjectTypeSelectorSection />
       </main>
-
-      <footer className="flex flex-col w-[395px] items-start gap-12 pt-12 pb-32 px-8 relative mt-12 bg-transparent rounded-[16px_16px_0px_0px] overflow-hidden border-[none] shadow-[inset_1.13e-16px_1.85px_1.85px_#ffffff,inset_-1.13e-16px_-1.85px_1.85px_#ebebeb] backdrop-blur-[15px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(15px)_brightness(100%)] bg-[linear-gradient(142deg,rgba(255,255,255,0.1)_0%,rgba(255,255,255,0)_100%)] before:content-[''] before:absolute before:inset-0 before:p-px before:rounded-[16px_16px_0px_0px] before:[background:linear-gradient(172deg,rgba(255,255,255,0)_0%,rgba(170,127,251,1)_37%,rgba(170,127,251,1)_70%,rgba(255,255,255,0)_100%)] before:[-webkit-mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)] before:[-webkit-mask-composite:xor] before:[mask-composite:exclude] before:z-[1] before:pointer-events-none">
-        <div className="flex items-center gap-2 relative self-stretch w-full flex-[0_0_auto]">
-          <img
-            className="relative w-[219px] h-[60px] object-cover"
-            alt="Logo principal"
-            src="/logo-principal-violet-7c5ed6-2x-1.png"
-          />
-        </div>
-      </footer>
     </div>
   );
 };
