0bbfc97d288bcb7c50736f1bedae2600d83e5c23
jndolou <j.ndolou@gmail.com>
2025-11-20 10:31:44 +0100

    Updated AppLayout.tsx

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import { FooterMenu } from "./FooterMenu";
 
 interface AppLayoutProps {
   children: ReactNode;
@@ -9,7 +8,6 @@ export const AppLayout = ({ children }: AppLayoutProps): JSX.Element => {
   return (
     <div className="min-h-screen flex flex-col">
       <main className="flex-1">{children}</main>
-      <FooterMenu />
     </div>
   );
 };
