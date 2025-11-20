5b59efa9bef0027243fd297a61e47e522e23af13
jndolou <j.ndolou@gmail.com>
2025-11-20 13:19:06 +0100

    Updated Performance.tsx

--- src/screens/Performance/Performance.tsx
+++ src/screens/Performance/Performance.tsx
@@ -494,8 +494,6 @@ export const Performance = (): JSX.Element => {
             </p>
           </CardContent>
         </Card>
-
-        <ExpertiseSection showGoogleRating={true} paddingTop="pt-8" paddingBottom="pb-0" />
       </main>
 
       <footer className="flex flex-col items-start gap-12 pt-12 pb-32 px-8 bg-transparent rounded-[16px_16px_0px_0px] overflow-hidden border-[none] shadow-[inset_1.13e-16px_1.85px_1.85px_#ffffff,inset_-1.13e-16px_-1.85px_1.85px_#ebebeb] backdrop-blur-[15px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(15px)_brightness(100%)] bg-[linear-gradient(142deg,rgba(255,255,255,0.1)_0%,rgba(255,255,255,0)_100%)] before:content-[''] before:absolute before:inset-0 before:p-px before:rounded-[16px_16px_0px_0px] before:[background:linear-gradient(172deg,rgba(255,255,255,0)_0%,rgba(170,127,251,1)_37%,rgba(170,127,251,1)_70%,rgba(255,255,255,0)_100%)] before:[-webkit-mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)] before:[-webkit-mask-composite:xor] before:[mask-composite:exclude] before:z-[1] before:pointer-events-none">
