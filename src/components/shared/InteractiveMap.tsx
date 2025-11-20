f17e2db9a79fa49445a76dd8dafd64a8404e7b1a
jndolou <j.ndolou@gmail.com>
2025-11-20 16:21:50 +0100

    Updated InteractiveMap.tsx

--- src/components/shared/InteractiveMap.tsx
+++ src/components/shared/InteractiveMap.tsx
@@ -46,7 +46,7 @@ export const InteractiveMap: React.FC<InteractiveMapProps> = ({
       L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
         maxZoom: 20,
         subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-        attribution: ''
+        attribution: '© Google'
       }).addTo(map);
 
       const marker = L.marker(center).addTo(map);
